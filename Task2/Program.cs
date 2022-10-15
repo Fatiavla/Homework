6df35f8e85ae9bff7bd9ffc3edaf58a2305986cf
Fatiavla <bioafc@mail.ru>
2022-10-15 13:14:18 +0300

    compl

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -7,3 +7,14 @@
 */
 
 Console.WriteLine("Напишите 3 числа");
+string a = Console.ReadLine();
+string b = Console.ReadLine();
+string c = Console.ReadLine();
+int numberA = int.Parse(a);
+int numberB = int.Parse(b);
+int numberC = int.Parse(c);
+int max = numberA;
+if(numberB > max) max = numberB;
+if(numberC > max) max = numberC;
+
+Console.WriteLine(max);
