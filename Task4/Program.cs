96cc90635969a8089b7d535b2ceec8515dfcdec5
Fatiavla <bioafc@mail.ru>
2022-10-15 14:05:44 +0300

    oshibka

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -4,3 +4,12 @@
 8 -> 2, 4, 6, 8
 */
 
+Console.WriteLine("напишите число: ");
+string NumberA = Console.ReadLine();
+int N = int.Parse(NumberA);
+
+for (int x = N; x<=N; x++)
+{  
+    if(x%2==0)
+   Console.WriteLine($"{x}");
+}
