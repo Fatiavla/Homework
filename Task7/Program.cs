a61fb21315fe068639296998659fed39cd0dc011
Fatiavla <bioafc@mail.ru>
2022-10-16 13:02:10 +0300

    task7compl

--- Task7/Program.cs
+++ Task7/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.Write("Напишите цифру от 1 до 7: ");
+string number = Console.ReadLine();
+int dayNumber = int.Parse(number);
+
+void Checkday (int daynumber) // Checkday не забыть вывести в итог
+{
+    if(daynumber == 6 || daynumber == 7)
+    {
+        Console.Write("да");
+    }
+    else if (daynumber < 1 || daynumber > 7)
+    {
+        Console.Write("такого дня нет");
+    }
+    else Console.Write("нет");
+
+}
+
+Checkday(dayNumber); // вот сюда, иначе работать не будет
