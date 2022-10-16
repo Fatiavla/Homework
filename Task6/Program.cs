40d5e53a135db89c0e60854dd98b7a481ef2a3b4
Fatiavla <bioafc@mail.ru>
2022-10-16 12:47:38 +0300

    task6comp

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -7,6 +7,13 @@
 32679 -> 6
 */
 
-Console.WriteLine("Введите число: ");
-int number = int.Parse(Console.ReadLine());
-
+Console.Write("Введи число: ");
+string Newnumber = Console.ReadLine();
+int number = int.Parse(Newnumber);
+if (Newnumber.Length > 2)
+{
+  Console.WriteLine(Newnumber[2]);
+}
+else {
+  Console.WriteLine("третьей цифры нет");
+}
